a17b70c3f0a7ad7a3e7f79ba48dacbb9162bfabf
ZephyrusZhang <2996362441@qq.com>
2023-06-11 16:39:41 +0800

    feat: page transitions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from "@chakra-ui/react"
 import Layout from '../components/layouts/main'
 import theme from "../lib/theme"
+import { AnimatePresence } from "framer-motion"
 
 const App = ({ Component, pageProps, router }: AppProps) => {
   return (
     <ChakraProvider theme={theme}>
       <Layout router={router}/>
-      <Component {...pageProps} key={router.route}/>
+      <AnimatePresence mode='wait' initial>
+        <Component {...pageProps} key={router.route}/>
+      </AnimatePresence>
     </ChakraProvider>
   )
 }
