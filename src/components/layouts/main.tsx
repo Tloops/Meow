9fb039a76c8a9e81f9312e0018f519220a1c7b8d
ZephyrusZhang <2996362441@qq.com>
2023-06-11 13:54:05 +0800

    feat: main layout

---
+++ src/components/layouts/main.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Box, Container } from "@chakra-ui/react"
+import Head from "next/head"
+import Navbar from '../navbar'
+
+const Main = ({ children, router }: any) => {
+  return (
+    <Box as='main' pb={8}>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1'/>
+        <title>Meow</title>
+      </Head>
+
+      <Navbar path={router.asPath}/>
+
+      <Container maxW='container.md' pt={14}>
+        {children}
+      </Container>
+    </Box>
+  )
+}
+
+export default Main
