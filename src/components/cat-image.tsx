0b3c2c6cc4afcdec05aaf23250e0b987d56135c4
ZephyrusZhang <2996362441@qq.com>
2023-06-12 11:25:07 +0800

    style: `cat-image` and `cat-video` components' horizontal margin

--- src/components/cat-image.tsx
+++ src/components/cat-image.tsx
@@ -8,9 +8,13 @@ interface Props {
 }
 
 const CatImage = ({ src, alt, caption }: Props) => (
-  <Box>
-    <Image src={src} alt={alt} borderRadius='lg' w='full' mb={4}/>
-    <Text textAlign='center'>
+  <Box
+    my={4}
+    // @ts-ignore
+    align='center'
+  >
+    <Image src={src} alt={alt} borderRadius='lg' w='full'/>
+    <Text textAlign='center' as='sub'>
       {caption}
     </Text>
   </Box>
--- src/components/cat-video.tsx
+++ src/components/cat-video.tsx
@@ -8,11 +8,19 @@ interface Props {
 
 const CatVideo = ({ src, caption }: Props) => {
   return (
-    <Box w='full' borderRadius='lg' overflow='hidden' mb={4}>
+    <Box
+      w='full'
+      borderRadius='lg'
+      overflow='hidden'
+      // @ts-ignore
+      align='center'
+      py={4}
+      mb={4}
+    >
       <video controls>
         <source src={src} type='video/mp4'/>
       </video>
-      <Text textAlign='center'>
+      <Text textAlign='center' as='sub'>
         {caption}
       </Text>
     </Box>
