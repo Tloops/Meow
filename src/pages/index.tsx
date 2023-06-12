3c0f6decca9e8215bfacebbcbd67cd84651f7947
ZephyrusZhang <2996362441@qq.com>
2023-06-12 11:36:42 +0800

    feat: main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,29 @@
-import { Box, Container } from "@chakra-ui/react"
+import { Box, Container, Heading, useColorModeValue } from "@chakra-ui/react"
 import Layout from "@/components/layouts/article"
+import Section from "@/components/section"
+import Paragraph from "@/components/paragraph"
 
 
 export default function Home() {
   return (
     <Layout>
       <Container>
-        <Box borderRadius='lg' bg='red' p={3} mb={6} textAlign='center'>
+        <Box
+          borderRadius='lg'
+          p={3}
+          mb={6}
+          textAlign='center'
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          css={{ backdropFilter: 'blur(10px)' }}
+        >
           妮可的猫猫们！
         </Box>
+
+        <Section delay={0.1}>
+          <Paragraph>
+            该网站收录了在深圳南山区西丽桃源街区1088号校园内生活过的一些猫猫的档案记录。
+          </Paragraph>
+        </Section>
       </Container>
     </Layout>
   )
