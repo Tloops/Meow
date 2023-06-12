3523de4a44199dbc302831ce933a6d793fb39184
ZephyrusZhang <2996362441@qq.com>
2023-06-12 20:10:49 +0800

    feat: makou's page

--- src/pages/cats/makou.tsx
+++ src/pages/cats/makou.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Container, Heading, List, ListItem } from "@chakra-ui/react"
+import { Container, Heading, HStack, List, ListItem } from "@chakra-ui/react"
 import Layout from "@/components/layouts/article"
 import SimpleBreadcrumb from "@/components/simple-breadcrumb"
 import CatImage from "@/components/cat-image"
 import Section from "@/components/section"
 import Paragraph from "@/components/paragraph"
 import Meta from "@/components/meta"
+import CatVideo from "@/components/cat-video"
 
 const Makou = () => {
   return (
@@ -36,7 +37,7 @@ const Makou = () => {
             </ListItem>
           </List>
         </Section>
-        <Section delay={0.1}>
+        <Section delay={0.2}>
           <Heading as='h3' variant='section-title'>
             刷新地点
           </Heading>
@@ -44,7 +45,7 @@ const Makou = () => {
             一教一楼；一教二楼
           </Paragraph>
         </Section>
-        <Section delay={0.1}>
+        <Section delay={0.3}>
           <Heading as='h3' variant='section-title'>
             起源
           </Heading>
@@ -52,6 +53,31 @@ const Makou = () => {
             麻酱最初于2019年在学校中出现。发现她的时候已经怀有身孕。之后产下整整九只猫崽，并且在猫舍同学的帮助下，这九只崽子全部存活并被送养。
           </Paragraph>
           <CatImage src='/images/cats/makou/makou-0.jpg' alt='麻酱带娃' caption='麻酱的九只崽'/>
+          <CatVideo src='/videos/makou/0.mp4'/>
+          <Paragraph>
+            带完崽绝育后，麻酱就开启了“我吃吃吃”的状态。几个月内吃成了肥猪（
+          </Paragraph>
+          <HStack>
+            <CatImage src='/images/cats/makou/makou-1.jpg' alt='痩麻酱' caption='胖之前'/>
+            <CatImage src='/images/cats/makou/makou-2.jpg' alt='飞竹' caption='”飞竹“'/>
+          </HStack>
+          <Paragraph>
+            还诞生了下面的名场面
+          </Paragraph>
+          <CatVideo src='/videos/makou/1.mp4'/>
+          <Paragraph>
+            虽然麻酱平时性格不错，给撸给亲亲，但是她有时候抽风也会抓人（麻酱应该是校内发卡发的最多的猫猫）。所以各位平时撸的时候要小心。
+          </Paragraph>
+        </Section>
+        <Section delay={0.4}>
+          <Heading as='h3' variant='section-title'>
+            云吸猫
+          </Heading>
+          <CatImage src='/images/cats/makou/makou-3.jpg' alt='麻酱'/>
+          <CatImage src='/images/cats/makou/makou-4.jpg' alt='麻酱'/>
+          <CatImage src='/images/cats/makou/makou-5.jpg' alt='麻酱'/>
+          <CatImage src='/images/cats/makou/makou-6.jpeg' alt='麻酱'/>
+          <CatImage src='/images/cats/makou/makou-7.jpg' alt='麻酱'/>
         </Section>
       </Container>
     </Layout>
