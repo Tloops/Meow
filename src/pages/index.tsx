1a75f0b17074cead712e33be83c8f54a573911d5
ZephyrusZhang <2996362441@qq.com>
2023-06-12 11:44:19 +0800

    feat: main page copyright announcement

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,6 +24,14 @@ export default function Home() {
             该网站收录了在深圳南山区西丽桃源街区1088号校园内生活过的一些猫猫的档案记录。
           </Paragraph>
         </Section>
+        <Section delay={0.2}>
+          <Heading as='h3' variant='section-title'>
+            声明
+          </Heading>
+          <Paragraph>
+            由于网站中使用了许多来源比较难以追溯的图片，所以在搭建时并没有直接向图片的原作者申请使用授权（摆了）。如有侵权，请联系 <b>QQ：2996362441</b> 进行删除。
+          </Paragraph>
+        </Section>
       </Container>
     </Layout>
   )
